e86a9d48bb483531b470680eb797fd4b1f80dab8
licongyi <licongyi@wecash.net>
2021-02-20 12:02:57 +0800

    send email with attach result

--- monix-app-test/utility/email_unti.py
+++ monix-app-test/utility/email_unti.py
@@ -50,13 +50,38 @@ class SendEmail:
         pass_result = "%.2f%%" % (pass_num / count_num * 100)
         fail_result = "%.2f%%" % (fail_num / count_num * 100)
 
-        content = "运行结果个数为%s个\n通过个数为%s个\n失败个数为%s个\n通过率为%s\n失败率为%s" % (
+        content = "运行结果个数为%s个\n通过个数为%s个\n失败个数为%s个\n通过率为%s\n失败率为%s\n\n详情请参考附件测试结果:" % (
             count_num, pass_num, fail_num, pass_result, fail_result)
-        self.send_mail(global_config.receiver, sub, content)
+        self.send_attach_mail(global_config.receiver, sub, content)
+
+    def send_attach_mail(self, receiveList, sub, content):
+        # 设置邮件基本内容
+        msg = MIMEMultipart()  # 创建一个带附件的实例
+        msg['Subject'] = sub  # 设置邮件标题
+        msg['From'] = formataddr(["licongyi", global_config.sender])  # 发件人名称
+        msg['To'] = ";".join(receiveList)  # 收件人列表
+        msg.attach(MIMEText(content, "plain", "utf-8"))  # 邮件正文内容
+
+        # 邮件携带附件
+        filename = "../testcase/testcase.xls"  # 保存的报告路径和名称
+        att1 = MIMEText(open(filename, 'rb').read(), 'base64', 'utf-8')
+        att1["Content-Type"] = 'application/octet-stream'
+        att1["Content-Disposition"] = 'attachment; filename="testcase.xls"'  # 这里的filename可以任意写，是邮件中附件显示的名字
+        msg.attach(att1)  # 添加附件，如果有多个附件，同理添加
+
+        # 发送邮件
+        server = smtplib.SMTP_SSL("smtp.qq.com", 465)
+        server.login(global_config.sender, global_config.my_pass)
+        server.sendmail(global_config.sender, receiveList, msg.as_string())
+        print("邮件发送成功" + str(receiveList))
+        server.quit()
 
 
 if __name__ == '__main__':
     s = SendEmail()
+    content="测试报告见附件"
+    sub="report"
+    # s.send_attach_mail(global_config.receiver,sub,content)
     pass_list = [1, 1, 1, 1, 1, 1, 1, 1]
     fail_list = [1, 1]
     s.send_main(pass_list, fail_list)
